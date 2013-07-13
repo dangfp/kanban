43c7a5d14cb03e3549aac7492c98b5a5379a578d
ruoshuigao <jannegao@gmail.com>
2013-07-13 15:57:11 +0800

    finished workfolw

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,4 +1,25 @@
 class Issue < ActiveRecord::Base
+  include Workflow
+
+  workflow_column :testing_status
+  workflow do
+    state :new do
+      event :start_developing, :transitions_to => :in_developing
+    end
+    state :in_developing do
+      event :self_test, :transitions_to => :self_tested
+    end
+    state :self_tested do
+      event :pass, :transitions_to => :success
+      event :fail, :transitions_to => :failed
+      event :report, :transitions_to => :NA
+    end
+    state :success
+    state :failed do
+      event :restart, :transitions_to => :new
+    end
+    state :NA
+  end
   belongs_to :feature
   belongs_to :developer
   belongs_to :tester
