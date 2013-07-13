1423bd412aa648dbce7510f77eafb61e79b98c33
ruoshuigao <jannegao@gmail.com>
2013-07-13 14:38:52 +0800

    create project and feature

---
+++ app/models/feature.rb
@@ -0,0 +1,4 @@
+class Feature < ActiveRecord::Base
+  belongs_to :project
+  attr_accessible :name
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kanban::Application.config.secret_token = '5fbe2b2c7103a8b4f5160e13d2dc4e74ec040e8e50810f322ffec23abc4d871646a66c828d6efd51981244e78e64f220c041425ed2a283905e250ecc0305fe1b'
