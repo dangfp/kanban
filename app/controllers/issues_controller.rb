aea2043ea81445ce986c5694496c60f93da7cbe6
ruoshuigao <jannegao@gmail.com>
2013-07-16 07:17:02 +0800

    create issue

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,5 +1,20 @@
+#encoding: utf-8
 class IssuesController < ApplicationController
   def index
-    @issues = Issue.paginate(page: params[30])
+    @issues = Issue.paginate(page: params[:page])
+  end
+
+  def new
+    @issue = Issue.new
+  end
+
+  def create
+    @issue = Issue.new(params[:issue])
+
+    if @issue.save
+      redirect_to @issue, notice: '测试用例添加成功.'
+    else
+      render action: 'new'
+    end
   end
 end
