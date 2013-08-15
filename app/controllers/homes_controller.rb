8031f8f2f2ebd8bd330506f1b0b9117af4a0a6be
ruoshui gao <jannegao@gmail.com>
2013-08-15 09:34:46 +0800

    add test statis

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -4,26 +4,35 @@ class HomesController < ApplicationController
     @projects = Project.all
 
     @results = {}
-    #Project.all.each do |project|
-    @results = Project.all.map {|project| {  project_name: project.name, 
-                                             issues_count_of_project: project.issues.count,
-                                             new_issues_count_of_project: project.issues.where("self_testing_status = ?" , '未开发').count,
-                                             new_issues_rate: percents(project.issues.where("self_testing_status = ?" , '未开发').count, project.issues.count),
-                                             in_development_issues_count_of_project: project.issues.where("self_testing_status = ?" , '开发中').count,
-                                             in_development_issues_rate: percents(project.issues.where("self_testing_status = ?" , '开发中').count, project.issues.count),
-                                             self_tested_issues_count_of_project: project.issues.where("self_testing_status = ?", '自测通过').count,
-                                             self_tested_issues_rate: percents(project.issues.where("self_testing_status = ?", '自测通过').count, project.issues.count) 
-                                         }
-                                     }
-    #end
+    @results = Project.all.map {|project|
+                                   issues_count_of_project = project.issues.count
 
-    #  @result[issues_count_of_project] = project.issues.count
-    #  @result[new_issues_count_of_project] = project.issues.where("self_testing_status = ?" , '未开发').count
-    #  @result[in_development_issues_count_of_project] = project.issues.where("self_testing_status = ?", '开发中').count
-    #  @result[self_tested_issues_count_of_project] = project.issues.where("self_testing_status = ?", '自测通过').count
-    #  @percentage_of_new_issues = (@new_issues_count_of_project.round(2) / @issues_count_of_project.round(2) * 100).to_s + '%'
-    #  @percentage_of_in_development_issues = (@in_development_issues_count_of_project.round(2) / @issues_count_of_project.round(2) * 100).to_s + '%'
-    #  @percentage_of_self_tested_issues = (@self_tested_issues_count_of_project.round(2) / @issues_count_of_project.round(2) * 100).to_s + '%'
+                                   new_issues_count_of_project = project.issues.where("self_testing_status = ?" , '未开发').count
+                                   in_development_issues_count_of_project = project.issues.where("self_testing_status = ?" , '开发中').count
+                                   self_tested_issues_count_of_project = project.issues.where("self_testing_status = ?", '自测通过').count
+
+                                   not_test_issues_count_of_project = project.issues.where("testing_status = ?" , '未测试').count
+                                   tested_successful_issues_count_of_project = project.issues.where("testing_status = ?" , '测试通过').count
+                                   tested_fail_issues_count_of_project = project.issues.where("testing_status = ?" , '测试失败').count
+
+                                  {  project_name: project.name, 
+                                     issues_count_of_project: issues_count_of_project,
+
+                                     new_issues_count_of_project: new_issues_count_of_project,
+                                     new_issues_rate: percents(new_issues_count_of_project, issues_count_of_project),
+                                     in_development_issues_count_of_project: in_development_issues_count_of_project,
+                                     in_development_issues_rate: percents(in_development_issues_count_of_project, issues_count_of_project),
+                                     self_tested_issues_count_of_project: self_tested_issues_count_of_project,
+                                     self_tested_issues_rate: percents(self_tested_issues_count_of_project, issues_count_of_project), 
+                                  
+                                    not_test_issues_count_of_project: not_test_issues_count_of_project,
+                                    not_test_issues_rate: percents(not_test_issues_count_of_project, issues_count_of_project),
+                                    tested_successful_issues_count_of_project: tested_successful_issues_count_of_project,
+                                    tested_successful_issues_rate: percents(tested_successful_issues_count_of_project, issues_count_of_project),
+                                    tested_fail_issues_count_of_project: tested_fail_issues_count_of_project,
+                                    tested_fail_issues_rate: percents(tested_fail_issues_count_of_project, issues_count_of_project)
+                                   }
+                                 }
   end
 
   def percents(var1, var2)
