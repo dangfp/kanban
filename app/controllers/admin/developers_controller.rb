cbce91ebf5cf51b15c906c8dce2ae8b6a7555bed
ruoshui gao <jannegao@gmail.com>
2013-08-22 20:35:12 +0800

    create dashboard and in dashboard add developer index

--- app/controllers/admin/developers_controller.rb
+++ app/controllers/admin/developers_controller.rb
@@ -2,4 +2,24 @@ class Admin::DevelopersController < Admin::ApplicationController
   def index
     @developers = Developer.all
   end
+
+  def new
+    @developer = Developer.new
+  end
+
+  def create
+    @developer = Developer.new(params[:developer])
+    if @developer.save!
+       redirect_to admin_developers
+    else
+       render :new
+    end
+  end
+
+  def edit
+    @developer = Developer.find(params[:id])
+  end
+
+  def destroy
+  end
 end
