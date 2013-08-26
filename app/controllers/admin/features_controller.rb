f8509c62648077564e2744b9fc896e6554d590e0
ruoshui gao <jannegao@gmail.com>
2013-08-26 18:26:55 +0800

    dashboard creatd edit destroy feature is ok

--- app/controllers/admin/features_controller.rb
+++ app/controllers/admin/features_controller.rb
@@ -1,5 +1,44 @@
+#encoding: utf-8
 class Admin::FeaturesController < Admin::ApplicationController
   def index
-    @features = Feature.all
+
+    cookies[:project_id] = params[:project_id] unless params[:project_id] == nil
+    @features = Feature.where(project_id: cookies[:project_id])
+  end
+
+  def new
+    @feature = Feature.new
+  end
+
+  def create
+    @feature = Feature.new(params[:feature])
+    @feature.project_id = cookies[:project_id]
+
+    if @feature.save!
+      redirect_to admin_project_features_path(project_id: cookies[:project_id])
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @feature = Feature.find(params[:id])
+  end
+
+  def update
+    @feature = Feature.find(params[:id])
+    if @feature.update_attributes(params[:feature])
+      redirect_to admin_project_features_path(project_id: cookies[:project_id]), notice: '模块修改成功'
+    else
+      render 'edit'
+    end
+  end
+
+  
+
+  def destroy
+    @feature = Feature.find(params[:id])
+    @feature.destroy
+    redirect_to admin_project_features_path(project_id: cookies[:project_id])
   end
 end
